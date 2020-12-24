579e70e5307db57096cc40e83331b1bb75e2fda1
Kutsalaba <kutsalaba.nazariy@gmail.com>
2020-12-25 01:27:48 +0200

    Added files with practice 15

---
+++ Practice_15_KM-04_Kutsalaba/factorial/factorial.py
@@ -0,0 +1,20 @@
+
+"""This module calculates the factorial of a number"""
+
+def fact(factorial = 1):
+
+    try:
+        n = int(input("Enter a natural number: "))
+        if n >= 0:
+            while n > 1:
+                factorial *= n
+                n -= 1
+            print(f'Factorial of the entered number = {factorial}')
+            return factorial
+        else:
+            raise ValueError
+
+    except ValueError:
+        print("Error, enter the data correctly.")
+        return fact()
+
